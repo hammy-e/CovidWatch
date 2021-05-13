4e393c326c1ef5c99d7af1582dbe6ec49088aff5
hammy-e <63534623+hammy-e@users.noreply.github.com>
2021-05-13 01:16:37 -0700

    Update Country.swift

--- Covid Watch/Model/Country.swift
+++ Covid Watch/Model/Country.swift
@@ -32,10 +32,10 @@ struct Country {
             recovered = numberFormatter.string(from: NSNumber(value: covidData["US"]["All"]["recovered"].intValue))!
             deaths = numberFormatter.string(from: NSNumber(value: covidData["US"]["All"]["deaths"].intValue))!
             percentInfected = Country.intToPercentString(number: covidData["US"]["All"]["confirmed"].intValue, isWhatPercentOf: covidData["US"]["All"]["population"].intValue)
-            administered = numberFormatter.string(from: NSNumber(value: vaccineData[named]["All"]["administered"].intValue))!
-            fullyVaccinated = numberFormatter.string(from: NSNumber(value: vaccineData[named]["All"]["people_vaccinated"].intValue))!
-            partiallyVaccinated = numberFormatter.string(from: NSNumber(value: vaccineData[named]["All"]["people_partially_vaccinated"].intValue))!
-            percentVaccinated = Country.intToPercentString(number: vaccineData[named]["All"]["people_vaccinated"].intValue, isWhatPercentOf: covidData["US"]["All"]["population"].intValue)
+            administered = numberFormatter.string(from: NSNumber(value: vaccineData["US"]["All"]["administered"].intValue))!
+            fullyVaccinated = numberFormatter.string(from: NSNumber(value: vaccineData["US"]["All"]["people_vaccinated"].intValue))!
+            partiallyVaccinated = numberFormatter.string(from: NSNumber(value: vaccineData["US"]["All"]["people_partially_vaccinated"].intValue))!
+            percentVaccinated = Country.intToPercentString(number: vaccineData["US"]["All"]["people_vaccinated"].intValue, isWhatPercentOf: covidData["US"]["All"]["population"].intValue)
         } else {
             icon = COUNTRIESEMOJIS[named]!
             population = numberFormatter.string(from: NSNumber(value: covidData[named]["All"]["population"].intValue))!
